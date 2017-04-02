d399ccf216511987355d79eec3de518099fd8123
John Clements <clements@racket-lang.org>
2017-04-02 15:03:40 -0700

    lab 1 template code

---
+++ lab1.py
@@ -0,0 +1,33 @@
+#* Section 1 (Git)
+
+#* Section 2 (Data Definitions)
+
+#* 1)
+
+#* 2)
+
+#* 3)
+
+#* 4)
+
+#* Section 3 (Signature, Purpose Statements, Headers)
+
+#* 1)
+
+#* 2)
+
+#* 3)
+
+#* 4)
+
+#* Section 4 (Test Cases)
+
+#* 1)
+
+#* 2)
+
+#* 3)
+
+#* Section 5 (Test Cases with Mutation)
+
+#* 1)
